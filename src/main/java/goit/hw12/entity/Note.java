37ea1694be65940c355a06d935a89c3186a83a77
VitaliyJV <114652130+VitaliyJV@users.noreply.github.com>
2023-08-27 10:39:54 +0300

    2
    2

--- src/main/java/goit/hw12/entity/Note.java
+++ src/main/java/goit/hw12/entity/Note.java
@@ -2,12 +2,9 @@ package goit.hw12.entity;
 
 import lombok.Builder;
 import lombok.Data;
-import org.hibernate.annotations.Generated;
 
 @Data
-@Builder
 public class Note {
-    @Generated
     private long id;
     private String title;
     private String content;
